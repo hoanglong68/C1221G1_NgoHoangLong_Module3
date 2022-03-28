18d9d6b3c175912ceb09039f1439b065a8727988
Hoang Long <longhoang6668@gmail.com>
2022-03-28 10:49:11 +0700

    up bai

--- ss1_co_so_du_lieu_quan_he/bai_tap/tao_bang.sql
+++ ss1_co_so_du_lieu_quan_he/bai_tap/tao_bang.sql
@@ -1,12 +1,12 @@
 create table student_management.class(
-id int auto_increment not null,
-`name` varchar(45) null,
+id int auto_increment,
+`name` varchar(45),
 primary key (id)
 );
 create table student_management.teacher(
-id int auto_increment not null,
-`name` varchar(45) null,
-age int null,
-country varchar(45) null,
+id int auto_increment,
+`name` varchar(45),
+age int,
+country varchar(45),
 primary key(id)
 );
